d302963219733beba18a04a32cab2a10d4102397
yanpetrenko <yanppavlovich@gmail.com>
2022-01-22 19:54:55 +0300

    env setup

---
+++ src/cli.js
@@ -0,0 +1,6 @@
+/* eslint-disable linebreak-style */
+import readlineSync from 'readline-sync';
+
+console.log('Welcome to the Brain Games!');
+const userName = readlineSync.question('May I have your name? ');
+export default userName;
