37a9b979221e88505e6b1dddd33a6731ddc96b94
yanpetrenko <yanppavlovich@gmail.com>
2022-02-23 18:34:53 +0300

    last

--- src/game-src/brain-gcd-src.js
+++ src/game-src/brain-gcd-src.js
@@ -1,8 +1,8 @@
 /* eslint-disable linebreak-style */
 
 const brainGcd = () => {
-  const first = Math.floor(Math.random() * 100) + 25;
-  const second = Math.floor(Math.random() * 100) + 25;
+  const first = Math.floor(Math.random() * 20);
+  const second = Math.floor(Math.random() * 20);
   const question = `${first} ${second}`;
   let correctAnswer;
   for (let i = 25; i > 0; i -= 1) {
@@ -11,6 +11,6 @@ const brainGcd = () => {
       break;
     }
   }
-  return [question, correctAnswer];
+  return [question, String(correctAnswer)];
 };
 export default brainGcd;
